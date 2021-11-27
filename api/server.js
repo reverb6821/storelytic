771528390419f7f57844e0bcd2b011c3b43fc012
Giuseppe Gigliotti <giuseppegigliottidev@gmail.com>
2021-11-27 15:15:41 +0100

    fix logs

--- api/server.js
+++ api/server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
 const path = require('path');
+const morgan = require('morgan');
+const logger = require('./lib/winston')
 
 const app = express();
 const db = require('./models/sequelize'); //* sequelize model import
@@ -20,6 +22,8 @@ if (!process.env.PORT) {
 const PORT = parseInt(process.env.PORT);
 
 app.use(cors(corsOptions));
+app.use(morgan("combined", { stream: logger.stream.write })); //? morgan setted
+app.use(function(err, req, res, next) { logger.error(`${req.method} - ${err.message}  - ${req.originalUrl} - ${req.ip}`); next(err)}); //? custom logger used  
 app.use(express.json()); //? parse requests of content-type - application/json
 app.use(express.urlencoded({ extended: true })); //? parse requests of content-type - application/x-www-form-urlencoded
 app.use(express.static(path.resolve(__dirname, './view'))); //? express client directory
