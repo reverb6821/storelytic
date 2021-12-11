4a3d108bfae88c41403965ab3b6764ceec2edd04
reverb6821 <giuseppegigliottidev@gmail.com>
2021-12-11 19:01:47 +0100

    dashboard and login page done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
+import "antd/dist/antd.css";
+
+import Main from './Components/Main/Main'
+import Home from './view/Home'
+import Login from './view/Login'
+
 import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Router>
+        <Switch>
+          <Route exact path='/' component={Login} />
+            <Main>
+              <Route exact path='/dashboard' component={Home} />
+              <Redirect from='*' to='/dashboard' />
+            </Main>
+        </Switch> 
+      </Router>
     </div>
   );
 }
---
+++ client/src/view/Login.jsx
@@ -0,0 +1,127 @@
+import React, { Component } from "react";
+
+import {
+  Layout,
+  Menu,
+  Button,
+  Row,
+  Col,
+  Typography,
+  Form,
+  Input
+} from "antd";
+import signinbg from "../assets/images/img-signin.jpg";
+
+const { Title } = Typography;
+const { Header, Footer, Content } = Layout;
+
+
+export default class SignIn extends Component {
+  render() {
+    const onFinish = (values) => {
+      console.log("Success:", values);
+    };
+
+    const onFinishFailed = (errorInfo) => {
+      console.log("Failed:", errorInfo);
+    };
+    return (
+      <>
+        <Layout className="layout-default layout-signin">
+          <Header>
+            <div className="header-col header-brand">
+              <h5>Storelytic</h5>
+            </div>
+            <div className="header-col header-nav"/>
+            <div className="header-col header-btn">
+              <Button type="primary">FREE DOWNLOAD</Button>
+            </div>
+          </Header>
+          <Content className="signin">
+            <Row gutter={[24, 0]} justify="space-around">
+              <Col
+                xs={{ span: 24, offset: 0 }}
+                lg={{ span: 6, offset: 2 }}
+                md={{ span: 12 }}
+              >
+                <Title className="mb-15">Sign In</Title>
+                <Title className="font-regular text-muted" level={5}>
+                  Enter your email and password to sign in
+                </Title>
+                <Form
+                  onFinish={onFinish}
+                  onFinishFailed={onFinishFailed}
+                  layout="vertical"
+                  className="row-col"
+                >
+                  <Form.Item
+                    className="username"
+                    label="Email"
+                    name="email"
+                    rules={[
+                      {
+                        required: true,
+                        message: "Please input your email!",
+                      },
+                    ]}
+                  >
+                    <Input placeholder="Email" />
+                  </Form.Item>
+
+                  <Form.Item
+                    className="username"
+                    label="Password"
+                    name="password"
+                    rules={[
+                      {
+                        required: true,
+                        message: "Please input your password!",
+                      },
+                    ]}
+                  >
+                    <Input placeholder="Password" />
+                  </Form.Item>
+
+                  <Form.Item>
+                    <Button
+                      type="primary"
+                      htmlType="submit"
+                      style={{ width: "100%" }}
+                    >
+                      SIGN IN
+                    </Button>
+                  </Form.Item>
+                  <p className="font-semibold text-muted">
+                  
+                    ALERT ERRORE
+                  </p>
+                </Form>
+              </Col>
+              <Col
+                className="sign-img"
+                style={{ padding: 12 }}
+                xs={{ span: 24 }}
+                lg={{ span: 12 }}
+                md={{ span: 12 }}
+              >
+                <img src={signinbg} alt="" />
+              </Col>
+            </Row>
+          </Content>
+          <Footer>
+            <Menu mode="horizontal">
+              <Menu.Item>Company</Menu.Item>
+              <Menu.Item>About Us</Menu.Item>
+              <Menu.Item>Pricing</Menu.Item>
+            </Menu>
+            <Menu mode="horizontal" className="menu-nav-social"/>
+            <p className="copyright">
+              {" "}
+              Copyright © 2021 Storelytic by <a href="#a">reverb6821</a>.{" "}
+            </p>
+          </Footer>
+        </Layout>
+      </>
+    );
+  }
+}
