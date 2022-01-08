95528e311debba0e6ca99fec6d30649d129c7d09
Giuseppe Gigliotti <giuseppegigliottidev@gmail.com>
2022-01-08 12:38:14 +0100

    fix roles access

--- api/src/routes/product.ts
+++ api/src/routes/product.ts
@@ -5,25 +5,25 @@ import { checkRole } from '../middlewares/checkRole';
 
 const productRouter = Router();
 
-productRouter.get('/productslist', [checkJwt, checkRole(['ADMIN'])], ProductController.listAll);
+productRouter.get('/productslist', [checkJwt, checkRole(['ADMIN', 'USER'])], ProductController.listAll);
 
 productRouter.get(
   '/product:id([0-9]+)',
-  [checkJwt, checkRole(['ADMIN'])],
+  [checkJwt, checkRole(['ADMIN', 'USER'])],
   ProductController.getOneById,
 );
 
-productRouter.post('/newproduct', [checkJwt, checkRole(['ADMIN'])], ProductController.newProduct);
+productRouter.post('/newproduct', [checkJwt, checkRole(['ADMIN', 'USER'])], ProductController.newProduct);
 
 productRouter.patch(
   '/editproduct:id([0-9]+)',
-  [checkJwt, checkRole(['ADMIN'])],
+  [checkJwt, checkRole(['ADMIN', 'USER'])],
   ProductController.editProduct,
 );
 
 productRouter.delete(
   '/deleteproduct:id([0-9]+)',
-  [checkJwt, checkRole(['ADMIN'])],
+  [checkJwt, checkRole(['ADMIN', 'USER'])],
   ProductController.deleteProduct,
 );
 
