a68098a7122530b6cfbc8f50b5e5fdb63890ccd1
Giuseppe Gigliotti <giuseppegigliottidev@gmail.com>
2022-01-09 17:45:58 +0100

    added company controller

---
+++ api/src/controller/CompanyController.ts
@@ -0,0 +1,104 @@
+import { Request, Response } from 'express';
+import { getRepository } from 'typeorm';
+import { validate } from 'class-validator';
+
+import Logger from '../../lib/logger';
+import { Company } from '../entity/Company';
+
+class CompanyController {
+  static listAll = async (req: Request, res: Response) => {
+    const companyRepository = getRepository(Company);
+    const company = await companyRepository.find({
+      select: ['companyId', 'title', 'description', 'logourl', 'note'],
+    });
+    res.send(company);
+  };
+
+  static getOneById = async (req: Request, res: Response) => {
+    const { companyId } = req.params;
+
+    const companyRepository = getRepository(Company);
+    try {
+      const company = await companyRepository.findOneOrFail(companyId, {
+        select: ['companyId', 'title', 'description', 'logourl', 'note'], // We dont want to send the password on response
+      });
+      res.send(company);
+    } catch (error) {
+      Logger.error(res.status(404).send('company not found'));
+    }
+  };
+
+  static newCompany = async (req: Request, res: Response) => {
+    const {
+      title, description, logourl, note,
+    } = req.body;
+    const company = new Company();
+    company.title = title;
+    company.description = description;
+    company.logourl = logourl;
+    company.note = note;
+
+    const errors = await validate(company);
+    if (errors.length > 0) {
+      Logger.error(res.status(400).send(errors));
+      return;
+    }
+
+    Logger.info(res.status(201).send('company created'));
+  };
+
+  static editCompany = async (req: Request, res: Response) => {
+    const { companyId } = req.params;
+    const {
+      title, description, logourl, note,
+    } = req.body;
+    const companyRepository = getRepository(Company);
+    let company;
+
+    try {
+      company = await companyRepository.findOneOrFail(companyId);
+    } catch (error) {
+      // If not found, send a 404 response
+      Logger.error(res.status(404).send('company not found'));
+      return;
+    }
+
+    company.title = title;
+    company.description = description;
+    company.logourl = logourl;
+    company.note = note;
+
+    const errors = await validate(company);
+    if (errors.length > 0) {
+      Logger.error(res.status(400).send(errors));
+      return;
+    }
+
+    try {
+      await companyRepository.save(company);
+    } catch (e) {
+      Logger.warn(res.status(409).send('nmname already in use'));
+      return;
+    }
+    // After all send a 204 (no content, but accepted) response
+    Logger.info(res.status(204).send());
+  };
+
+  static deleteCompany = async (req: Request, res: Response) => {
+    const { companyId } = req.params;
+
+    const companyRepository = getRepository(Company);
+    let company: Company;
+    try {
+      company = await companyRepository.findOneOrFail(companyId);
+    } catch (error) {
+      Logger.error(res.status(404).send('company not found'));
+      return;
+    }
+    companyRepository.delete(companyId);
+
+    Logger.info(res.status(204).send());
+  };
+}
+
+export default CompanyController;
---
+++ api/src/routes/company.ts
@@ -0,0 +1,30 @@
+import { Router } from 'express';
+import { checkJwt } from '../middlewares/checkJwt';
+import { checkRole } from '../middlewares/checkRole';
+import CompanyController from '../controller/CompanyController';
+
+const companyRouter = Router();
+
+companyRouter.get('/companieslist', [checkJwt, checkRole(['ADMIN', 'USER'])], CompanyController.listAll);
+
+companyRouter.get(
+  '/company:id([0-9]+)',
+  [checkJwt, checkRole(['ADMIN', 'USER'])],
+  CompanyController.getOneById,
+);
+
+companyRouter.post('/newcompany', [checkJwt, checkRole(['ADMIN'])], CompanyController.newCompany);
+
+companyRouter.patch(
+  '/editcompany:id([0-9]+)',
+  [checkJwt, checkRole(['ADMIN'])],
+  CompanyController.editCompany,
+);
+
+companyRouter.delete(
+  '/deletecompany:id([0-9]+)',
+  [checkJwt, checkRole(['ADMIN'])],
+  CompanyController.deleteCompany,
+);
+
+export default companyRouter;
