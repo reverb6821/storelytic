84e4a4af258d7e04cecc7cdc31baa69a6e24a028
Giuseppe Gigliotti <giuseppegigliottidev@gmail.com>
2022-02-19 23:54:17 +0100

    added husky and client side

---
+++ webapp/.eslintrc.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+const path = require('path');
+
+const prettierOptions = JSON.parse(
+  fs.readFileSync(path.resolve(__dirname, '.prettierrc'), 'utf8'),
+);
+
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ['plugin:react/recommended', 'airbnb', 'prettier'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react', 'prettier'],
+  rules: {
+    'prettier/prettier': ['error', prettierOptions],
+    'react/react-in-jsx-scope': 'off',
+  },
+};
