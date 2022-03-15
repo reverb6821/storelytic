e5d22269aba99d38f35e1564cb6f557a27ddfc69
reverb6821 <giuseppegigliottidev@gmail.com>
2022-03-15 22:58:37 +0100

    added swagger

--- api/server.ts
+++ api/server.ts
@@ -5,49 +5,53 @@ import * as dotenv from 'dotenv';
 import helmet from 'helmet';
 import cors from 'cors';
 import express from 'express';
+import swaggerUi from 'swagger-ui-express';
+import * as swaggerDocument from './swagger.json';
 import { router } from './src/routes/index'; //! router import
 import * as errorHandler from './src/middlewares/errorHandler'; //! handler error import
 import morganMiddleware from './src/middlewares/morganMiddleware'; //! custom middleware for morgan
 import Logger from './lib/logger'; //! custom logger builded with winston
 
+const app = express();
+
+dotenv.config(); //* import settings from .env file
+
+if (!process.env.PORT) {
+  process.exit(1);
+}
+
+const PORT: number = parseInt(process.env.PORT as string, 10);
+
+app.use(helmet());
+app.use(cors());
+app.use(morganMiddleware);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// app.use(errorHandler.notFound) //? http status handler
+app.use(errorHandler.internalServerError);
+
+app.use('/logger', (_, res) => {
+  Logger.error('ERROR : ');
+  Logger.warn('WARNING : ');
+  Logger.info('INFO : ');
+  Logger.http('HTTP LOG : ');
+  Logger.debug('DEBUG : ');
+});
+
+// ? swagger config
+app.use(
+  '/v2/swagger.io/api-docs',
+  swaggerUi.serve,
+  swaggerUi.setup(swaggerDocument),
+);
+
+app.use(express.static(path.resolve(__dirname, '../dist/client'))); // ? this will make react app visible to server when builded
+
+app.use('/', router); //* express use the routes form router file
+
 createConnection()
   .then(async (connection: Connection) => {
-    dotenv.config(); //* import settings from .env file
-
-    if (!process.env.PORT) {
-      process.exit(1);
-    }
-
-    const PORT: number = parseInt(process.env.PORT as string, 10);
-
-    const app = express(); //* express declaration
-
-    app.use(helmet());
-    app.use(cors());
-    app.use(morganMiddleware);
-    app.use(express.json());
-    app.use(express.urlencoded({ extended: true }));
-
-    // app.use(errorHandler.notFound) //? http status handler
-    app.use(errorHandler.internalServerError);
-
-    app.use('/logger', (_, res) => {
-      Logger.error('ERROR : ');
-      Logger.warn('WARNING : ');
-      Logger.info('INFO : ');
-      Logger.http('HTTP LOG : ');
-      Logger.debug('DEBUG : ');
-    });
-
-    app.use(express.static(path.resolve(__dirname, '../dist/client'))); // ? this will make react app visible to server when builded
-
-    app.use('/', router); //* express use the routes form router file
-
-    // ? All other GET requests not handled before will return our React app
-    // app.get('*', (req, res) => {
-    //     res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
-    // });
-
     // ? server activation
     app.listen(PORT, () => {
       Logger.info(`Listening on port ${PORT}`);
