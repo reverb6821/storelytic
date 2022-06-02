e3e21db63837af07f140b43c77cb53d31175d744
reverb6821 <giuseppegigliottidev@gmail.com>
2022-06-02 19:27:50 +0200

    added nav and dashboard

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { DashboardComponent } from './views/dashboard/dashboard.component';
+const routes: Routes = [{ path: 'home', component: DashboardComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
