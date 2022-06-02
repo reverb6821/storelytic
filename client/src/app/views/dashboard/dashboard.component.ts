3c545ea2c31a9dd89209b34e8cd786800ff1c8b8
reverb6821 <giuseppegigliottidev@gmail.com>
2022-06-02 22:56:52 +0200

    added cards

--- client/src/app/views/dashboard/dashboard.component.ts
+++ client/src/app/views/dashboard/dashboard.component.ts
@@ -12,22 +12,24 @@ export class DashboardComponent {
   cards = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
     map(({ matches }) => {
       if (matches) {
-        return [
-          { title: 'Card 1', cols: 1, rows: 1 },
-          { title: 'Card 2', cols: 1, rows: 1 },
-          { title: 'Card 3', cols: 1, rows: 1 },
-          { title: 'Card 4', cols: 1, rows: 1 }
-        ];
+        return {
+          columns: 1,
+          miniCard: { cols: 1, rows: 1 },
+          chart: { cols: 1, rows: 2 },
+          table: { cols: 1, rows: 4 },
+        };
       }
 
-      return [
-        { title: 'Card 1', cols: 2, rows: 1 },
-        { title: 'Card 2', cols: 1, rows: 1 },
-        { title: 'Card 3', cols: 1, rows: 2 },
-        { title: 'Card 4', cols: 1, rows: 1 }
-      ];
+      return {
+        columns: 4,
+        miniCard: { cols: 1, rows: 1 },
+        chart: { cols: 2, rows: 2 },
+        table: { cols: 4, rows: 4 },
+      };
     })
   );
+
+  
 
   constructor(private breakpointObserver: BreakpointObserver) {}
 }
