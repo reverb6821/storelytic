d651b987a3f93d8176762a52546f8fd9d7c85dcb
reverb6821 <giuseppegigliottidev@gmail.com>
2022-09-01 17:35:59 +0200

    update

--- client/src/views/Register.js
+++ client/src/views/Register.js
@@ -124,11 +124,6 @@ export default class Register extends Component {
     return (
       <div className="col-md-12">
         <div className="card card-container">
-          <img
-            src="//ssl.gstatic.com/accounts/ui/avatar_2x.png"
-            alt="profile-img"
-            className="profile-img-card"
-          />
 
           <Form
             onSubmit={this.handleRegister}
