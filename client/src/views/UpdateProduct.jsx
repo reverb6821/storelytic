c5543afd69a870cdcce2cdc86db76266dff5a18e
reverb6821 <giuseppegigliottidev@gmail.com>
2022-09-20 23:47:29 +0200

    fix update

--- client/src/views/UpdateProduct.jsx
+++ client/src/views/UpdateProduct.jsx
@@ -19,8 +19,8 @@ const UpdateProduct = (props) => {
   const { id } = useParams();
   const dispatch = useDispatch();
 
-  const getProduct = id => {
-    productService.get({ id: currentProduct.id })
+  const getProduct = async() => {
+   await productService.get(id)
       .then(response => {
         setCurrentProduct(response.data);
       })
@@ -34,8 +34,8 @@ const UpdateProduct = (props) => {
   // }, [props.match.params.id]);
 
   useEffect(() => {
-    if (id)
-        getProduct(id);
+    
+      getProduct(id);
 }, [id]);
 
   const handleInputChange = event => {
@@ -93,7 +93,7 @@ const UpdateProduct = (props) => {
 
                 <div>
                   <label htmlFor="name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Product Name</label>
-                  <input type="text" name="name" id="name" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  <input type="text" name="name" id="name" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                     placeholder="{currentProduct.name}"
                     required=""
                     value={currentProduct.name}
@@ -107,7 +107,7 @@ const UpdateProduct = (props) => {
                     name="description" 
                     id="description" 
                     placeholder="{currentProduct.description} "
-                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                     required=""
                     
                     value={currentProduct.description}
@@ -141,12 +141,12 @@ const UpdateProduct = (props) => {
               )}
               <button
                 type="submit"
-                className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+                className="w-full text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                 onClick={updateContent}
               >
                 Update
               </button>
-              <Link to={"/product/"} className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">
+              <Link to={"/product/"} className="w-full text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                 Cancel
               </Link>
 
